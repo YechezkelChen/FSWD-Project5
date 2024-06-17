638c8d64a18c1a536e5475c2c63e4d3d81431f1c
Daniel Attali <danielattali16@gmail.com>
2024-06-17 08:33:15 +0300

    Added styles to the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import "./styles/Pages.css";
+import "./styles/Home.css";
+
 export default function Home() {
     return (
-        <>
-        <h1>Home</h1>
-        <p>Welcome to the Home page</p>
-        </>
+        <div className="main">
+        <h1 className="page-header">Home</h1>
+        <p className="page-intro">Welcome to the Home page</p>
+        {/* <p>This app contains</p> */}
+        <ul className="home-list">
+            <li>Posts</li>
+            <li>Comments</li>
+            <li>Users</li>
+            <li>Albums</li>
+            <li>Photos</li>
+        </ul>
+        </div>
     );
 }
